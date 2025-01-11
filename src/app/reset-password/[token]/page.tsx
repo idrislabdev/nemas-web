3213853a75157cc9b8d72494ed72403dfca8e3c0
mochamad <inoreds@gmail.com>
2025-01-11 18:29:48 +0700

    update

--- src/app/reset-password/[token]/page.tsx
+++ src/app/reset-password/[token]/page.tsx
@@ -1,7 +1,12 @@
+"use client"
+import { useParams } from 'next/navigation'
 import React from 'react'
 
-export const page = () => {
-  return (
-    <div>page</div>
-  )
+const ResetPasswordToken = () => {
+    const params = useParams<{ token: string }>()
+    return (
+        <div>{params.token}</div>
+    )
 }
+
+export default ResetPasswordToken
