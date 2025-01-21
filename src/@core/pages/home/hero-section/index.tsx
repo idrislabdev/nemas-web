5b0dc8dc9b8ed6976290bb581c47d8fee1b026b6
mochamad <inoreds@gmail.com>
2025-01-21 20:48:38 +0700

    update

--- src/@core/pages/home/hero-section/index.tsx
+++ src/@core/pages/home/hero-section/index.tsx
@@ -18,19 +18,19 @@ const HomeHeroSection = () => {
                 modules={[Pagination]}
             >
                 <SwiperSlide>
-                    <div className='slide-container' style={{ backgroundImage: `url(/images/swipers/hero-big.png)`, width: '100%',height: '100%', backgroundSize: 'cover'}}>
+                    <div className='slide-container'>
                         <h5>Investasi <span>EMAS</span> Tanpa Batas Dengan <span>NEMAS</span></h5>
                         <button>Beli Emas Sekarang!</button>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className='slide-container' style={{ backgroundImage: `url(/images/swipers/hero-big.png)`, width: '100%',height: '100%', backgroundSize: 'cover'}}>
+                    <div className='slide-container'>
                         <h5>Investasi <span>EMAS</span> Tanpa Batas Dengan <span>NEMAS</span></h5>
                         <button>Beli Emas Sekarang!</button>
                     </div>
                 </SwiperSlide>
                 <SwiperSlide>
-                    <div className='slide-container' style={{ backgroundImage: `url(/images/swipers/hero-big.png)`, width: '100%',height: '100%', backgroundSize: 'cover'}}>
+                    <div className='slide-container'>
                         <h5>Investasi <span>EMAS</span> Tanpa Batas Dengan <span>NEMAS</span></h5>
                         <button>Beli Emas Sekarang!</button>
                     </div>
