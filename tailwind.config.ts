e8b639fc3c0336cb8dccfc25433556cbbb7e8023
mochamad <inoreds@gmail.com>
2025-03-03 21:40:43 +0700

    update

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,7 +19,7 @@ const config: Config = {
         "2xl": { max: "1535px" },  
         xl: { max: "1279px" },  
         lg: { max: "1023px" },  
-        md: { max: "767px" },  
+        md: { max: "820px" },  
         sm: { max: "639px" },
       },
       fontFamily: {
