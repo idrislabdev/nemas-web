9e3adef2c2deefbad4fccc2d09a58087865a251d
mochamad <inoreds@gmail.com>
2025-03-08 09:38:29 +0700

    update

--- src/app/akun/pengaturan/page.tsx
+++ src/app/akun/pengaturan/page.tsx
@@ -1,53 +1,57 @@
+"use client"
+import { useGlobals } from '@/@core/hoc/useGlobals'
 import { Trash01 } from '@untitled-ui/icons-react'
 import { Input } from 'antd'
 import Image from 'next/image'
 import React from 'react'
 
 const UserPengaturanPage = () => {
-  return (
-    <div className='pengaturan-sub-container'>
-        <h5>Pengaturan Akun</h5>
-        <div className='profil-area'>
-            <div className='sub-area'>
-                <div className='form-area'>
-                    <h6>Data Pribadi</h6>
-                    <div className='group-input'>
-                        <label>Nama Akun</label>
-                        <Input size='large' />
-                    </div>
-                    <div className='group-input'>
-                        <label>No Handphone</label>
-                        <Input size='large' />
-                    </div>
-                    <div className='group-input'>
-                        <label>Email</label>
-                        <Input size='large' />
-                    </div>
-                </div>
-            </div>
-            <div className='playstore-area'>
-                <div className='playstore'>
-                    <div className='playstore-content'>
-                        <div className='image-area'>
-                            <div className='image-circle'></div>
-                            <Image src={`/images/smartphone-mockup.png`} alt='smartphone mockup' width={0} height={0} sizes='100%' />
+    const { globals } = useGlobals()
+    
+    return (
+        <div className='pengaturan-sub-container'>
+            <h5>Pengaturan Akun</h5>
+            <div className='profil-area'>
+                <div className='sub-area'>
+                    <div className='form-area'>
+                        <h6>Data Pribadi</h6>
+                        <div className='group-input'>
+                            <label>Nama Akun</label>
+                            <Input size='large' value={globals.userLogin.name} disabled className='disabled:bg-white disabled:text-neutral-700' />
                         </div>
-                        <p>
-                            Lakukan Pendaftaran & Verifikasi Dokumen seperti KTP & Nomor Rekening di <span>Aplikasi Nemas</span>
-                        </p>
+                        <div className='group-input'>
+                            <label>No Handphone</label>
+                            <Input size='large' value={globals.userLogin.phone_number} disabled className='disabled:bg-white disabled:text-neutral-700'/>
+                        </div>
+                        <div className='group-input'>
+                            <label>Email</label>
+                            <Input size='large' value={globals.userLogin.email} disabled className='disabled:bg-white disabled:text-neutral-700'/>
+                        </div>
                     </div>
-                    <div className='playstore-footer'>
-                        <a><Image src={`/images/playstore.png`} className="w-full h-full" width={0} height={0} sizes='100%' alt='plastore' /></a>
-                        <a><Image src={`/images/appstore.png`} className="w-full h-full" width={0} height={0} sizes='100%' alt='appstore' /></a>
+                </div>
+                <div className='playstore-area'>
+                    <div className='playstore'>
+                        <div className='playstore-content'>
+                            <div className='image-area'>
+                                <div className='image-circle'></div>
+                                <Image src={`/images/smartphone-mockup.png`} alt='smartphone mockup' width={0} height={0} sizes='100%' />
+                            </div>
+                            <p>
+                                Lakukan Pendaftaran & Verifikasi Dokumen seperti KTP & Nomor Rekening di <span>Aplikasi Nemas</span>
+                            </p>
+                        </div>
+                        <div className='playstore-footer'>
+                            <a><Image src={`/images/playstore.png`} className="w-full h-full" width={0} height={0} sizes='100%' alt='plastore' /></a>
+                            <a><Image src={`/images/appstore.png`} className="w-full h-full" width={0} height={0} sizes='100%' alt='appstore' /></a>
+                        </div>
                     </div>
                 </div>
             </div>
+            <button className='btn-hapus'>
+                <Trash01 /> Hapus Akun
+            </button>
         </div>
-        <button className='btn-hapus'>
-            <Trash01 /> Hapus Akun
-        </button>
-    </div>
-  )
+    )
 }
 
 export default UserPengaturanPage
--- src/app/keranjang/page.tsx
+++ src/app/keranjang/page.tsx
@@ -1,15 +1,20 @@
+"use client";
+
 import Footer from '@/@core/components/footer'
 import MainHeader from '@/@core/components/main-header'
 import KeranjangPageWrapper from '@/@core/pages/keranjang'
 import React from 'react'
 import '@/styles/keranjang.css'
+import { GlobalsProvider } from '@/@core/context/globalContext'
 
 const KeranjangPage = () => {
   return (
     <>
-    <MainHeader />
-    <KeranjangPageWrapper />
-    <Footer />
+    <GlobalsProvider>
+      <MainHeader />
+      <KeranjangPageWrapper />
+      <Footer />
+    </GlobalsProvider>
   </>
   )
 }
